6a7cf9cdd0672904372f78b997c3bd76a96f33da
Alfredo <alfredonzk@gmail.com>
2023-08-14 21:48:54 -0300

    stitches config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default App
